ae69f89c7d736d1122c85332c6f562d5d77a88c4
Inha Bae <baeinha030603@gmail.com>
2024-10-30 13:35:39 -0700

    Update BrowseSolution.py
    - pickle module no longer used
    - removed saving feature
    - display_solution() directly called after cfr

--- BrowseSolution.py
+++ BrowseSolution.py
@@ -1,72 +1,29 @@
-import pickle
+import re
 
+INITIAL_HOLDINGS = {'11', '12', '13', '14', 
+                '22', '23', '24',
+                '33', '34', '44'}
 
-ACTION_NAMES = ['INCOME', 'ASSASSINATE', 'COUP', 'PASS', 'CHALLENGE', 'BLOCK ASSASSINATION']
-
-class SolutionDisplay():
-    def __init__(self, solutions):
-        self.solutions = solutions
-        self.current_history = ''
-        
-    def move_forward(self, next_action):
-        self.current_history += next_action
-
-    def move_backward(self):
-        self.current_history = self.current_history[:-1]
-
-    def display_solution(self):
-        for card_solution in self.solutions[self.current_history]:
-            for card, strategies in card_solution.items():
-                print(f"<{card}>")
-                for action_name, action_freq in strategies.items():
-                    print(f"\t{ACTION_NAMES[action_name]}: {action_freq}")
-
-    def round_solutions(self, ndigits):
-        for card_solutions in self.solutions.values():
-            for card_solution in card_solutions:
-                for strategies in card_solution.values():
-                    for action_name, action_freq in strategies.items():
-                        strategies[action_name] = round(action_freq, ndigits)
-
-
-if __name__ == "__main__":
-    with open('solutions.pkl', 'rb') as file:
-        solutions = pickle.load(file)
-    sd = SolutionDisplay(solutions)
-    sd.round_solutions(3) # strategies are rounded to 3 decimal places
-
+def display_solution(solutions, game_tree):
+    history = ""
     while True:
-        print("Kuhn Coup Solver")
-        print("---History--------------------")
-        print(sd.current_history)
-        print("------------------------------")
-        print("---Available Actions----------")
-        available_actions = []
-        for strategy in sd.solutions[sd.current_history]:
-            for action_strategy in strategy.values():
-                for action in action_strategy.keys():
-                    if action not in available_actions: 
-                        available_actions.append(action)
+        print("\nGAME HISTORY: ", history)
+        matching_keys = [key for key in game_tree if re.fullmatch(fr'^\d+{re.escape(history)}$', key)]
+        if not history:
+            matching_keys = list(INITIAL_HOLDINGS)
 
-        print([ACTION_NAMES[action] for action in available_actions])
-        print("------------------------------")
-        print("---Solutions------------------")
-        sd.display_solution()
-        print("------------------------------")
-        
-        print("Enter an action you would like to take (one length string).")
-        user_input = input("Or enter 'z' to undo the most recent action: ")
+        if not matching_keys: 
+            print("Terminal or Unexplored.")
+            break
 
-        assert len(user_input) == 1
+        for key in matching_keys:
+            print(key, end=": ")
+            if not solutions.get(key, None):
+                print("No Solution")
+            else:
+                solutions[key].print_average_strategy()
+                print()
 
-        if user_input == 'z':
-            sd.move_backward()
-        else:
-            sd.move_forward(user_input)
+        ac = input("\nNext action: (e.g. 'In'): ")
 
-
-        print()
-        print()
-
-
-        #TODO: stop at terminal node instead of error
+        history += ac
